e669e199cd20f15afc4c8a88840c6c0c525718f2
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-02-11 14:11:15 +0100

    weatherURL

--- main.js
+++ main.js
@@ -1,13 +1,71 @@
 "use strict";
+const searchBtn = document.querySelector('#submit');
+const searchBar = document.querySelector('#searchbar')
+const options = document.getElementsByName('opt');
+
+searchBtn.addEventListener('click', executeSearch);
+
+function executeSearch() {
+    let option = 'false';
+    for(let i = 0; i < options.length; i++){
+        if(options[i].checked){
+            console.log(options[i].value)
+            option = options[i].value;
+        }
+    }
+    let searchValues = {
+        city: searchBar.value,
+        filter: option
+    }
+
+}
+
+function getWeatherUrl(city){
+    const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+    const searchParams = {
+        q: city,
+        appid: '8a8cc7486f4d3b161979f954e21e692b',
+        units: 'metric',
+        lang: 'sv'
+    }
+    for(let prop in searchParams){
+        url.searchParams.append(prop, searchParams[prop]);
+    }
+    return url;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let CLIENT_ID = 'BMF55ZEHJXLKTPUKURRRCZLFUDKQDLNOOCBSSMQ3PJLPBYEU';
 let CLIENT_SECRET = 'X5YGNPMAFTLHFGYSG5SEN0B4FRRBX0HK5DXGCPOSALX1F4PT';
 let today = getDate();
 console.log(today);
 let foursquareGET = `https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${today}`;
-
-function executeSearch(){
-
-}
 
 function getDate(){
     let date = new Date();
@@ -23,5 +81,7 @@ function getDate(){
     }
 
     let today = year.toString() + month.toString() + day.toString();
-    return today;
+
+'¨ä    return today;
 }
+*/
