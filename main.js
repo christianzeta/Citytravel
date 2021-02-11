0c53624a25a942acd3f790b43472a181a3c9db96
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-02-11 15:04:38 +0100

    Working weather update

--- main.js
+++ main.js
@@ -2,8 +2,16 @@
 const searchBtn = document.querySelector('#submit');
 const searchBar = document.querySelector('#searchbar')
 const options = document.getElementsByName('opt');
+const cityTitle = document.querySelector('#city-title');
+const temp = document.querySelector('#temp');
+const condition = document.querySelector('#condition');
+const weatherDay = document.querySelector('#weather-day')
 
 searchBtn.addEventListener('click', executeSearch);
+
+function changeCityTitle(city){
+    cityTitle.innerText = city;
+}
 
 function executeSearch() {
     let option = 'false';
@@ -18,6 +26,8 @@ function executeSearch() {
         filter: option
     }
 
+    changeCityTitle(searchValues.city);
+    getWeatherInfo(searchValues.city, displayWeather);
 }
 
 function getWeatherUrl(city){
@@ -34,6 +44,57 @@ function getWeatherUrl(city){
     return url;
 }
 
+function getWeatherInfo(city, displayWeather){
+    let url = getWeatherUrl(city);
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+
+    xhr.onload = () => {
+        displayWeather(xhr.response);
+    }
+    xhr.send();
+}
+
+function displayWeather(info){
+    weatherDay.innerText = getDate('day');
+    temp.innerText = info.main.temp;
+    condition.innerText = info.weather[0].description;
+}
+
+function getDate(format){
+    let date = new Date();
+    const weekDay = {
+        0: 'Sunday',
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday'
+    }
+
+    let dayOfWeek = weekDay[date.getDay()];
+ 
+    let month = date.getMonth() + 1; 
+    let day = date.getDate();
+    let year = date.getFullYear();
+
+    if(month < 10){
+        month = '0' + month.toString();
+    }
+    if(day < 10){
+        day = '0' + day.toString();
+    }
+
+    let YMDay = year.toString() + month + day;
+
+    if(format = 'day'){
+        return dayOfWeek;
+    } else if(format = '¨YMDay'){
+        return YMDay;
+    }
+}
 
 
 
@@ -67,21 +128,4 @@ let today = getDate();
 console.log(today);
 let foursquareGET = `https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${today}`;
 
-function getDate(){
-    let date = new Date();
-    let month = date.getUTCMonth() + 1; 
-    let day = date.getUTCDate();
-    let year = date.getUTCFullYear();
-
-    if(month < 10){
-        month = '0' + month.toString();
-    }
-    if(day < 10){
-        day = '0' + day.toString();
-    }
-
-    let today = year.toString() + month.toString() + day.toString();
-
-'¨ä    return today;
-}
 */
